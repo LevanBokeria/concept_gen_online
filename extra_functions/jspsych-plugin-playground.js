ae134e1d9c2e893f02f9be59d044f5cd371119b1
Levan Bokeria <levan.bokeria@gmail.com>
2020-07-14 00:06:39 +0400

    updated the playgroun plugin to be modeled after free sort plugin. Defined an ineer box that will help me position the stimuli.

--- extra_functions/jspsych-plugin-playground.js
+++ extra_functions/jspsych-plugin-playground.js
@@ -6,21 +6,68 @@ jsPsych.plugins["plugin-playground"] = (function() {
 
   var plugin = {};
 
+  jsPsych.pluginAPI.registerPreload('plugin-playground', 'stimuli', 'image');
+
   plugin.info = {
     name: "plugin-playground",
     description: "",
     parameters: {
-      targetStim: {
-        type: jsPsych.plugins.parameterType.IMAGE,
-        pretty_name: 'Stimulus',
+      stimuli: {
+        type: jsPsych.plugins.parameterType.STRING,
+        pretty_name: 'Stimuli',
         default: undefined,
-        description: 'The image to be displayed'
+        array: true,
+        description: 'Images to be displayed.'
+      },
+      stim_height: {
+        type: jsPsych.plugins.parameterType.INT,
+        pretty_name: 'Stimulus height',
+        default: 100,
+        description: 'Height of images in pixels.'
+      },
+      stim_width: {
+        type: jsPsych.plugins.parameterType.INT,
+        pretty_name: 'Stimulus width',
+        default: 100,
+        description: 'Width of images in pixels'
+      },
+
+      stim_x_coords: {
+        type: jsPsych.plugins.parameterType.INT,
+        pretty_name: 'Stimulus x coordinates',
+        default: 100,
+        description: 'X coordinate of images.'
+      },
+      stim_y_coords: {
+        type: jsPsych.plugins.parameterType.INT,
+        pretty_name: 'Stimulus y coordinates',
+        default: 100,
+        description: 'Y coordinates of images'
+      },
+      sort_area_height: {
+        type: jsPsych.plugins.parameterType.INT,
+        pretty_name: 'Sort area height',
+        default: 800,
+        description: 'The height of the container that subjects can move the stimuli in.'
+      },
+      sort_area_width: {
+        type: jsPsych.plugins.parameterType.INT,
+        pretty_name: 'Sort area width',
+        default: 800,
+        description: 'The width of the container that subjects can move the stimuli in.'
       },
       prompt: {
         type: jsPsych.plugins.parameterType.STRING,
         pretty_name: 'Prompt',
         default: null,
-        description: 'Any content here will be displayed below the stimulus.'
+        description: 'It can be used to provide a reminder about the action the subject is supposed to take.'
+      },
+      prompt_location: {
+        type: jsPsych.plugins.parameterType.SELECT,
+        pretty_name: 'Prompt location',
+        options: ['above','below'],
+        default: 'above',
+        description: 'Indicates whether to show prompt "above" or "below" the sorting area.'
       },
       trial_duration: {
         type: jsPsych.plugins.parameterType.INT,
@@ -33,36 +80,64 @@ jsPsych.plugins["plugin-playground"] = (function() {
 
   plugin.trial = function(display_element, trial) {
 
-    // Display the target stimulu\
-    html = '<img src="'+trial.targetStim+'" id="jspsych-plugin-playground-target-stimulus" style="';
-    
-    if(trial.stimulus_height !== null){
-      html += 'height:'+trial.stimulus_height+'px; '
-      if(trial.stimulus_width == null && trial.maintain_aspect_ratio){
-        html += 'width: auto; ';
-      }
-    }
-    if(trial.stimulus_width !== null){
-      html += 'width:'+trial.stimulus_width+'px; '
-      if(trial.stimulus_height == null && trial.maintain_aspect_ratio){
-        html += 'height: auto; ';
-      }
-    }
+    var html = "";
+    // // check if there is a prompt and if it is shown above
+    // if (trial.prompt !== null && trial.prompt_location == "above") {
+    //   html += trial.prompt;
+    // }
 
-      // Add positioning for the image
-      html += 'position: absolute; left: ' + bellPosition.left + 'px; top: ' + bellPosition.top + 'px ';
+    html += '<div '+
+      'id="jspsych-free-sort-arena" '+
+      'class="jspsych-free-sort-arena" '+
+      'style="position: relative; width:'+trial.sort_area_width+'px; height:'+trial.sort_area_height+'px; border:2px solid #444;"'+
+      '></div>';
 
-    html +='"></img>';
+    // // check if prompt exists and if it is shown below
+    // if (trial.prompt !== null && trial.prompt_location == "below") {
+    //   html += trial.prompt;
+    // }
 
-    // Show prompt at the top
-    if (trial.prompt !== null) {
-      html += trial.prompt;
-    }
-    
-
-    
     display_element.innerHTML = html;
 
+    // store initial location data
+    var init_locations = [];
+
+    debugger;
+
+    const prompt = document.createElement('P')
+    prompt.innerText = trial.prompt
+    display_element.querySelector("#jspsych-free-sort-arena").appendChild(prompt)
+
+    for (var i = 0; i < trial.stimuli.length; i++) {
+      // var coords = random_coordinate(trial.sort_area_width - trial.stim_width, trial.sort_area_height - trial.stim_height);
+
+      const image = document.createElement('img')
+      image.src = trial.stimuli[i]
+      image.dataset.src = trial.stimuli[i]
+      // image.draggable = false
+      // image.classList.add('jspsych-free-sort-draggable')
+
+      display_element.querySelector("#jspsych-free-sort-arena").appendChild(image)
+
+      width = image.width = trial.stim_width[i]
+      height = image.height = trial.stim_height[i]
+      // var coords = random_coordinate(trial.sort_area_width - width, trial.sort_area_height - height);
+
+      var coords = {
+        x: trial.stim_x_coords[i],
+        y: trial.stim_y_coords[i]
+      }
+
+      image.style = 'position: absolute; top:'+coords.y+'px; left:'+coords.x+'px;'
+
+      init_locations.push({
+        "src": trial.stimuli[i],
+        "x": coords.x,
+        "y": coords.y
+      });
+    }
+
+    ////////////////////////////////////////////////////////////////////
     // store response
     var response = {
       rt: null,
@@ -71,47 +146,57 @@ jsPsych.plugins["plugin-playground"] = (function() {
 
     // ////////////////////////////////////////////// FUNCTIONS /////////////////////////////////////////////////////
 
-        // function to handle responses by the subject
-        var after_response = function(info) {
-    
-          // only record the first response
-          if (response.key == null) {
-            response = info;
-          }
-    
-          if (trial.response_ends_trial) {
-            end_trial();
-          }
-        };
+    // function to handle responses by the subject
+    var after_response = function(info) {
 
-        // function to end trial when it is time
-        var end_trial = function() {
+      // only record the first response
+      if (response.key == null) {
+        response = info;
+      }
 
-          // kill any remaining setTimeout handlers
-          jsPsych.pluginAPI.clearAllTimeouts();
+      if (trial.response_ends_trial) {
+        end_trial();
+      }
+    };
 
-          // kill keyboard listeners
-          if (typeof keyboardListener !== 'undefined') {
-            jsPsych.pluginAPI.cancelKeyboardResponse(keyboardListener);
-          }
+    // function to end trial when it is time
+    var end_trial = function() {
 
-          // gather the data to store for the trial
-          var trial_data = {
-            "rt": response.rt,
-            "stimulus": trial.targetStim,
-            "key_press": response.key
-          };
+      // kill any remaining setTimeout handlers
+      jsPsych.pluginAPI.clearAllTimeouts();
 
-          // clear the display
-          display_element.innerHTML = '';
+      // kill keyboard listeners
+      if (typeof keyboardListener !== 'undefined') {
+        jsPsych.pluginAPI.cancelKeyboardResponse(keyboardListener);
+      }
 
-          // move on to the next trial
-          jsPsych.finishTrial(trial_data);
-        };
+      // gather the data to store for the trial
+      var trial_data = {
+        "rt": response.rt,
+        "stimulus": trial.targetStim,
+        "key_press": response.key
+      };
+
+      // clear the display
+      display_element.innerHTML = '';
+
+      // move on to the next trial
+      jsPsych.finishTrial(trial_data);
+    };
+
+    // helper functions
+
+    function random_coordinate(max_width, max_height) {
+      var rnd_x = Math.floor(Math.random() * (max_width - 1));
+      var rnd_y = Math.floor(Math.random() * (max_height - 1));
+
+      return {
+        x: rnd_x,
+        y: rnd_y
+      };
+    }
 
     // //////////////////////////////////////////////////////////////////////////////////////////////////////////
-
-
 
 
     // start the response listener
