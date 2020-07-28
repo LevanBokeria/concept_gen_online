138f3bcef9a8a2d8508eaa36130811fc66bb58a8
Levan Bokeria <levan.bokeria@gmail.com>
2020-07-28 18:40:38 +0400

    trialCreator function is separate. Called to create trials for each phase.

--- extra_functions/custom_functions.js
+++ extra_functions/custom_functions.js
@@ -74,3 +74,70 @@ function add_image_info_and_trial_session_idxs_practice_trials(array,session_idx
   return array;
 
 };    
+
+let trialCreator = function(curr_space_object,baseTrialArray,targetCoordsNames,phase){
+
+    // Shuffle the base trial array
+    let baseTrialArrayInner = JSON.parse(JSON.stringify(shuffle(baseTrialArray)))
+
+    // Populate the baseTrialArrayInner with details for each trial
+    for(i = 0, j = 0; i < baseTrialArrayInner.length; i++, j+=2) {
+
+      // Randomize item1 and item2
+      if (i % 2 == 0){
+          [baseTrialArrayInner[i].item1, baseTrialArrayInner[i].item2] = [baseTrialArrayInner[i].item2, baseTrialArrayInner[i].item1]               
+      }
+
+      // Which one is the prompt item? If one of them is empty, prompt is the non-empty. Else, determine randomly
+      if (baseTrialArrayInner[i].item1.includes('E')){
+          baseTrialArrayInner[i].prompt_item_idx = 2
+      } else if (baseTrialArrayInner[i].item2.includes('E')){
+          baseTrialArrayInner[i].prompt_item_idx = 1
+      } else {
+          // Choose randomly
+          baseTrialArrayInner[i].prompt_item_idx = Math.floor(Math.random()*1) + 1
+      }
+      if (baseTrialArrayInner[i].prompt_item_idx == 1){
+          baseTrialArrayInner[i].foil_item_idx = 2
+      } else {
+          baseTrialArrayInner[i].foil_item_idx = 1
+      }
+
+
+
+      // img names and paths for items. Also point names
+      baseTrialArrayInner[i].item_img_names  = []
+      baseTrialArrayInner[i].item_img_paths  = []
+      baseTrialArrayInner[i].item_point_idxs = []
+      debugger
+      for (k=0; k<2; k++){
+          baseTrialArrayInner[i].item_img_names[k]  = targetCoordsNames.imgNamesUsed['phase_'+iPhase][targetCoordsNames.pointNamesUsed['phase_'+iPhase].indexOf(baseTrialArrayInner[i]['item'+(k+1)])] 
+          baseTrialArrayInner[i].item_img_paths[k]  = './img/targets/' + baseTrialArrayInner[i].item_img_names[k] + '.png'
+          baseTrialArrayInner[i].item_point_idxs[k] = targetCoordsNames.pointNamesUsed['phase_'+iPhase].indexOf(baseTrialArrayInner[i]['item'+(k+1)])+1
+      }
+
+      // point name of the prompt item?
+      baseTrialArrayInner[i].prompt_point_name = baseTrialArrayInner[i]["item" + baseTrialArrayInner[i].prompt_item_idx]
+      // target name of the prompt item?
+      baseTrialArrayInner[i].prompt_img_name = 
+          targetCoordsNames.imgNamesUsed['phase_'+iPhase][targetCoordsNames.pointNamesUsed['phase_'+iPhase].indexOf(baseTrialArrayInner[i].prompt_point_name)]
+      // prompt img path?
+      baseTrialArrayInner[i].prompt_img_path = './img/targets/' + baseTrialArrayInner[i].prompt_img_name + '.png'
+      // point name of the foil item?
+      baseTrialArrayInner[i].foil_point_name = baseTrialArrayInner[i]["item" + baseTrialArrayInner[i].foil_item_idx]
+      // target name of the foil item?
+      baseTrialArrayInner[i].foil_img_name = 
+          targetCoordsNames.imgNamesUsed['phase_'+iPhase][targetCoordsNames.pointNamesUsed['phase_'+iPhase].indexOf(baseTrialArrayInner[i].foil_point_name)]
+      // foil img path?
+      baseTrialArrayInner[i].foil_img_path = './img/targets/' + baseTrialArrayInner[i].foil_img_name + '.png'
+      // Exemplar img to load?
+      baseTrialArrayInner[i].ex_pairs_img_path = './img/' + curr_space_object.concept_space + '/pair_imgs_both_orders/pairs_' + 
+              baseTrialArrayInner[i].item_point_idxs[0] + '_' + baseTrialArrayInner[i].item_point_idxs[1] + '.png'            
+      // baseTrialArrayInner[i].ex_pairs_img_path = './img/' + curr_space_object.concept_space + '/pair_imgs_both_orders/pairs_16_16.png'                
+    }
+
+    // Shuffle once again, otherwise prompt item location is predictable 
+    baseTrialArrayInner = JSON.parse(JSON.stringify(shuffle(baseTrialArrayInner)))
+
+    return baseTrialArrayInner
+  }; // function trialCreator
